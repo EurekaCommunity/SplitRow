146c541a8feeada4cae84ba33c89428dc176a09d
Marco Betschart <marco@mandelkind.swiss>
2017-12-03 19:09:44 +0100

    Initial Commit

---
+++ SplitRow/SplitRow.h
@@ -0,0 +1,19 @@
+//
+//  SplitRow.h
+//  SplitRow
+//
+//  Created by Marco Betschart on 03.12.17.
+//  Copyright © 2017 MANDELKIND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SplitRow.
+FOUNDATION_EXPORT double SplitRowVersionNumber;
+
+//! Project version string for SplitRow.
+FOUNDATION_EXPORT const unsigned char SplitRowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SplitRow/PublicHeader.h>
+
+
